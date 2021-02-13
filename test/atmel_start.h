bcdbc2be9143364d285bcf39d1b6583283834970
lkvenild <lkvenild@gmail.com>
2021-02-13 13:54:40 +0100

    Update atmel_start.h

--- test/atmel_start.h
+++ test/atmel_start.h
@@ -1,1 +1,19 @@
-ost er også godt
+#ifndef ATMEL_START_H_INCLUDED
+#define ATMEL_START_H_INCLUDED
+//This file is a test from github
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "include/driver_init.h"
+#include "include/atmel_start_pins.h"
+
+/**
+ * Initializes MCU, drivers and middleware in the project
+ **/
+void atmel_start_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
